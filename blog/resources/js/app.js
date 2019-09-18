196eda7afec69c65a2d7d2749604e2e69600652c
kleitonalbuquerque <kleiton2102@gmail.com>
2019-09-18 11:33:24 -0300

    Shop config

--- blog/resources/js/app.js
+++ blog/resources/js/app.js
@@ -1,3 +1,16 @@
 require('./bootstrap');
+
+window.Vue = require('vue');
+import store from './store/index';
+import ElementUI from 'element-ui';
+import 'element-ui/lib/theme-chalk/index.css';
+
+Vue.use(ElementUI);
+
 Vue.component('create-post', require('./components/CreatePost.vue').default);
 Vue.component('all-posts', require('./components/AllPosts.vue'))
+
+const app = new Vue({
+  store,
+  el: '#app',
+})
